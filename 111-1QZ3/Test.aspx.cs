d9089135892fd395702377587f76fd9b1d0b511c
s9606105 <s9606105@gmail.com>
2022-11-25 07:52:02 +0800

    Write MID-2 to Test and Test_Sub, Update answer pdf and rar

---
+++ 111-1QZ3/Test.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ3
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        string[] s_City = new string[3] { "台北市", "新北市", "台中市" };
+        string[,] s_Area = new string[3, 3] {
+            {"中正區", "文山區", "大安區"},
+            {"淡水區", "石碇區", "土城區"},
+            {"西屯區", "北屯區", "東區"}
+        };
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                for (int i_x = 0; i_x < s_City.Length; i_x++)
+                {
+                    ListItem l_t = new ListItem();
+                    l_t.Text = l_t.Value = s_City[i_x];
+
+                    dpl_City.Items.Add(l_t);
+                }
+                set_City();
+            }
+        }
+
+        protected void dpl_City_TextChanged(object sender, EventArgs e)
+        {
+            set_City();
+        }
+
+        void set_City()
+        {
+            dpl_Area.Items.Clear();
+            int C_idx = dpl_City.SelectedIndex;
+            for (int i_y = 0; i_y < s_Area.GetLength(1); i_y++)
+            {
+                ListItem l_t = new ListItem();
+                l_t.Text = l_t.Value = s_Area[C_idx, i_y];
+
+                dpl_Area.Items.Add(l_t);
+            }
+        }
+
+        protected void rbl_Phone_TextChanged(object sender, EventArgs e)
+        {
+            string changTxt = rbl_Phone.Text;
+            if (changTxt == "無") txt_Phone.Visible = false;
+            else txt_Phone.Visible = true;
+        }
+    }
+}
---
+++ 111-1QZ3/Test_Sub.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _111_1QZ3
+{
+    public partial class Test_Sub : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string checkTxt = Request.Form.Get("rbl_Phone");
+            lb_Msg.Text =
+            "保單號碼: " + Request.Form.Get("tb_Num") + "<br />" +
+            "通訊種類: " + checkTxt + "<br />";
+
+            if (checkTxt != "無") lb_Msg.Text += "通訊號碼: " + Request.Form.Get("txt_Phone") + "<br />";
+
+            lb_Msg.Text +=
+            "所在城市: " + Request.Form.Get("dpl_City") + "<br />" +
+            "所在區域: " + Request.Form.Get("dpl_Area");
+        }
+    }
+}
